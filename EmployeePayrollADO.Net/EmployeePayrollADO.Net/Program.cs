533ce69e2f092bb84c2314f83301daeabeed1f5f
Balla <ballaphanindra@outlook.com>
2022-08-01 11:06:47 +0530

    Committing ADO.Net

---
+++ EmployeePayrollADO.Net/EmployeePayrollADO.Net/Program.cs
@@ -0,0 +1,24 @@
+﻿using EmployeePayrollADO.Net;
+using System;
+
+Console.WriteLine("Welcome to Employee Payroll!");
+EmployeeRepo employeeRepo = new EmployeeRepo();
+EmployeeModel employeeModel = new EmployeeModel();
+employeeModel.EmployeeName = "Bruce Wayne";
+employeeModel.PhoneNumber = "8524568962";
+employeeModel.Address = "12th Street";
+employeeModel.Department = "HR";
+employeeModel.Gender = 'M';
+employeeModel.BasicPay = 22000;
+employeeModel.Deductions = 1500;
+employeeModel.TaxablePay = 200;
+employeeModel.Tax = 300;
+employeeModel.NetPay = 25000;
+employeeModel.StartDate = Convert.ToDateTime("2021-10-05");
+employeeModel.City = "Mumbai";
+employeeModel.Country = "India";
+
+//if (repo.AddEmployee(employee))
+//    Console.WriteLine("Records added successfully");
+employeeRepo.GetAllEmployee();
+Console.ReadKey();
---
+++ EmployeePayrollADO.Net/EmployeePayrollADO.Net/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeePayrollADO.Net
+{
+    [TestClass]
+    internal class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeName = "Satish";
+            employee.Department = "Developer";
+            employee.PhoneNumber = "5826357895";
+            employee.Address = "Delhi";
+            employee.Gender = 'M';
+            employee.BasicPay = 15000.00M;
+            employee.Deductions = 200.00;
+            employee.StartDate = Convert.ToDateTime("2020-10-08");
+            //Mock<EmployeeRepo> mockObj = new Mock<EmployeeRepo>();
+            //mockObj.Setup(t=>t.AddEmployee(It.IsIn<EmployeeModel>)).return (true);
+            var result = repo.AddEmployee(employee);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetAllEmployeeShouldReturnListOfRecords()
+        {
+            EmployeeRepo repo = new EmployeeRepo();
+            var result = repo.GetAllEmployee();
+            Assert.IsTrue(result);
+        }
+    }
+}
